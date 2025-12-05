0d421756b127f87e65661dd1bc56a8fcf49d3689
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-12-05 20:09:53 +0300

    Correction du bug de trie et des tickets

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -146,7 +146,9 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`)
+      .off("click") // fixe remove existing event listener
+      .click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
